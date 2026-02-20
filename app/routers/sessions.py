55cc91594646d416409d9b47e6c797bf2b691faa
RickyUI <rtovarurbina@gmail.com>
2026-02-20 13:51:55 +0100

    Get all endpoint created

--- app/routers/sessions.py
+++ app/routers/sessions.py
@@ -3,11 +3,20 @@ from sqlalchemy.orm import Session
 from .. import models, schemas
 from ..services.ai_service import generate_summary
 from ..database import db_dependency
+from typing import List
 
 router = APIRouter()
 
+
+# Endpoint que devuelve todos los records de la DB
+@router.get("/sessions", response_model=List[schemas.StudySession], status_code=status.HTTP_200_OK)
+async def read_all_sessions(db: db_dependency):
+    return db.query(models.StudySession).all()
+
+    
+# Endpoint que recibe POST del usuario y almacena en la DB, ademas devuelve los valores agregados
 @router.post("/sessions", response_model=schemas.StudySession, status_code=status.HTTP_201_CREATED)
-def create_study_session(session: schemas.StudySessionCreate, db: db_dependency):
+async def create_study_session(session: schemas.StudySessionCreate, db: db_dependency):
     # Crear objeto de base de datos
     db_session = models.StudySession(**session.model_dump())
     db.add(db_session)
@@ -22,3 +31,5 @@ def create_study_session(session: schemas.StudySessionCreate, db: db_dependency)
 
     # Devolver la sesión completa
     return db_session
+
+
